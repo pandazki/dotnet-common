548ce3042a3bbe270a025c47ab6b9ae6b049b111
Pandazki <onlrrr@gmail.com>
2014-02-18 01:34:44 +0800

    add Pandazki.Common.Log.Simple Project

---
+++ Pandazki.Common.Log/Pandazki.Common.Log.Simple/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pandazki.Common.Log.Simple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //simple usage
+            Log.Critical("Critical");
+            Log.Information("Infomation");
+
+            //custom source
+            var log = LoggerFactory.GetLogger("CustomInfoSource");
+            log.Information("Custom Info");
+
+            Console.ReadKey();
+        }
+    }
+}
